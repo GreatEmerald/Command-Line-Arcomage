834d5bac1aebeca5acd0ce09249067a451352a86
GreatEmerald <pastas4@gmail.com>
2014-08-05 22:08:17 +0300

    Directory cleanup, addition of gitignore
    Version 1.alpha.02.01
    
    Signed-off-by: GreatEmerald <pastas4@gmail.com>

--- util/compile-x86_64.sh
+++ util/compile-x86_64.sh
@@ -2,5 +2,4 @@
 #rdmd -ILuaD -Isrc -L-llua --build-only -ofclarcomage clarcomage.d ../../libarcomage/include/libarcomage.a 
 cd ../src
 dmd -ILuaD -Isrc -L-llua -ofclarcomage clarcomage.d ../../libarcomage/src/*.d LuaD/luad/*.d LuaD/luad/conversions/*.d LuaD/luad/c/*.d
-mv clarcomage ../bin/pos64
-rm ../bin/pos64/*.o
+mv clarcomage ../bin/linux-x86_64
